f8da06a95bfee7819d4fbee5d54bc32b7100d697
ejonghwan <sun87-1@daum.net>
2022-01-02 23:30:45 +0900

    asd

---
+++ src/assets/js/index.js
@@ -0,0 +1,62 @@
+
+
+
+export const ui = (function(){
+
+
+    const eles = document.querySelectorAll('.item')
+
+    function tab() {
+        for(let i = 0; i < eles.length; i++) {
+            eles[i].querySelector('dt > button').addEventListener('click', function(e) {
+                if(!this.classList.contains('active')) {
+                    this.classList.add('active')
+                    eles[i].parentElement.querySelector('.code_wrap').classList.add('active')
+                    eles[i].parentElement.classList.add('on')
+                } else {
+                    this.classList.remove('active')
+                    eles[i].parentElement.querySelector('.code_wrap').classList.remove('active')
+                    eles[i].parentElement.classList.remove('on')
+                }
+                
+                // console.log(eles[i].parentElement)
+                // console.log(this)
+            })
+        }
+    }
+    tab();
+
+
+    const links = document.querySelectorAll('link')
+    const scripts = document.querySelectorAll('script')
+    const getDate = new Date()
+    const minute = getDate.getMinutes()
+    // console.log(minute)
+    function cash() {
+        for(let i = 0; i < links.length; i++) {
+            links[i].setAttribute('href', links[i].getAttribute('href') + `?ver${minute}`) 
+        }
+        for(let i = 0; i < scripts.length; i++) {
+            scripts[i].setAttribute('src', scripts[i].getAttribute('src') + `?ver${minute}`) 
+        }
+        // console.log(links)
+    }
+    cash();
+
+    
+
+
+   
+
+
+    return {
+        tab,
+        cash,
+    }
+
+
+
+})()
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+  mode: 'production', //production  devalopment  none
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js', //id chunkhash name
+    path: path.resolve(__dirname, 'dist'),
+    clean: {
+      keep: /\.git/,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader']
+        use: [ { loader: MiniCssExtractPlugin.loader },"css-loader" ]
+      },
+      {
+        test: /\.png|jpg|svg|gif$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name:'[name].[ext]?[hash]',
+            outputPath: './src/assets/images',
+          }
+        }]
+      },
+      {
+        test: /\.woff|woff2|eot|ttf|otf$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            // name:'[name].[ext]?[hash]',
+            outputPath: './src/assets/font',
+          }
+        }]
+      },
+      
+    ]
+  },
+  devServer: {
+    port: 9000,
+  }
+  ,
+  plugins: [
+    new MiniCssExtractPlugin(),
+    // new HtmlWebpackPlugin({ 
+    //   template: './index.html',
+    // }),
+
+    new HtmlWebpackPlugin({ 
+      template:'./index.html',
+      filename:'./index.html',
+    //   chunks:['main']
+      // chunks:['index'] //특정 js만 불러올수있게
+    }),
+    new HtmlWebpackPlugin({ 
+      template:'./src/pages/css/index.html',
+      filename:'./src/pages/css/index.html',
+      
+    }),
+    new HtmlWebpackPlugin({ 
+        template:'./src/pages/html/accessibility.html',
+        filename:'./src/pages/html/accessibility.html',
+   
+    }),
+    new HtmlWebpackPlugin({ 
+        template:'./src/pages/html/index.html',
+        filename:'./src/pages/html/index.html',
+ 
+      }),
+    new HtmlWebpackPlugin({ 
+        template:'./src/pages/html/standards.html',
+        filename:'./src/pages/html/standards.html',
+   
+      }),
+    new HtmlWebpackPlugin({ 
+        template:'./src/pages/js/index.html',
+        filename:'./src/pages/js/index.html',
+     
+      }),
+    new CleanWebpackPlugin(),
+  ],
+}
